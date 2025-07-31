21c81bf0347aa1be2c63da750d9998896bdf9374
umme-Tahazzee <tahazzeekaria1@gmail.com>
2025-07-31 23:50:48 +0600

    hero content

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -4,34 +4,40 @@ const Hero = () => {
  return (
   <section className="bg-main-bg" >
    <div className="hero-container" >
-    <img src="./images/hero-img.png"
-     alt="hero"
-     className="absolute bottom-0 left-1/2 -translate-x-1/2 object-auto scale-100
-              md:scale-150 "/>
-    <div className="hero-content">
-     <div className="overflow-hidden" >
-      <h1 className="hero-title">
-       Freaking Delicious</h1>
-     </div>
-     <div
-      style={{
-       // clipPath: "polygon(50% 0, 50% 0, 50% 100%, 50% 100%)",
-      }}
-      className="hero-text-scroll"
-     >
-      <div className="hero-subtitle">
-       <h1>Protein + Caffine </h1>
-      </div>
-     </div>
 
-    </div>
+   
+        <img
+              src="/images/static-img.png"
+              className="absolute bottom-0 left-1/2 -translate-x-1/2 object-auto"
+            />
 
 
+   <div className="hero-content">
+          <div className="overflow-hidden">
+            <h1 className="hero-title">Freaking Delicious</h1>
+          </div>
+          <div
+            style={{
+              // clipPath: "polygon(50% 0, 50% 0, 50% 100%, 50% 100%)",
+            }}
+            className="hero-text-scroll"
+          >
+            <div className="hero-subtitle">
+              <h1>Protein + Caffine </h1>
+            </div>
+          </div>
 
+          <h2>
+            Live life to the fullest  with SPYLT: Shatter boredom and embrace
+            your inner kid with every deliciously smooth chug.
+          </h2>
 
-
+          <div className="hero-button">
+            <p>Chug a SPYLT</p>
+          </div>
+        </div>
    </div>
-
+  
   </section>
  )
 }
