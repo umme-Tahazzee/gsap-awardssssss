997f10c3eb4c5c208d085671bbdbf4ca996575d8
umme-Tahazzee <tahazzeekaria1@gmail.com>
2025-07-31 23:16:18 +0600

    navbar hero

---
+++ src/app/page.js
@@ -0,0 +1,14 @@
+import Navbar from '@/components/Navbar'
+import React from 'react'
+import Hero from './../sections/Hero';
+
+const Home = () => {
+  return (
+    <main>
+       <Navbar/>
+       <Hero/>
+    </main>
+  )
+}
+
+export default Home
